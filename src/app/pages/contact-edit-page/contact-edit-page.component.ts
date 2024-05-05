30e90fb3958d7b14c88dce6c7b82e1a8902a1700
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-05 23:57:38 +0300

    Add and edit contact edit page to get data from params conditionally

--- src/app/pages/contact-edit-page/contact-edit-page.component.ts
+++ src/app/pages/contact-edit-page/contact-edit-page.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, inject } from '@angular/core';
+import { Contact } from '../../models/contact.model';
+import { Observable, filter, map } from 'rxjs';
+import { ContactService } from '../../services/contact.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'contact-edit-page',
   templateUrl: './contact-edit-page.component.html',
   styleUrl: './contact-edit-page.component.scss'
 })
-export class ContactEditPageComponent {
+
+export class ContactEditPageComponent implements OnInit {
+  private route = inject(ActivatedRoute)
+  private router = inject(Router)
+  private contactService = inject(ContactService)
+
+  contact = this.contactService.getEmptyContact()
+
+  ngOnInit(): void {
+    this.route.data.pipe(
+      map(data => data['contact']),
+      filter(contact => contact),
+    ).subscribe(contact => {
+      this.contact = contact
+    })
+  }
+
+  onSaveContact() {
+    this.contactService.saveContact(this.contact as Contact)
+      .subscribe({
+        next: () => this.router.navigateByUrl('/contact'),
+        error: err => console.log('err:', err)
+      })
+  }
+
 
 }
