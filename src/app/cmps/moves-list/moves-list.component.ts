e71d0c9165285ff70a910c53061ec1850850079c
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-06 20:16:44 +0300

    Add moveslist cmp

---
+++ src/app/cmps/moves-list/moves-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { Contact } from '../../models/contact.model';
+import { UserService } from '../../services/user.service';
+import { Move } from '../../models/user.model';
+
+@Component({
+  selector: 'moves-list',
+  templateUrl: './moves-list.component.html',
+  styleUrl: './moves-list.component.scss'
+})
+export class MovesListComponent implements OnInit {
+
+  @Input() contact!: Contact
+  private userService = inject(UserService)
+
+  user = this.userService.getUser()
+  moves: Move[] | null = null
+
+ngOnInit(): void {
+  
+}
+
+}
