0561380d3799c7d3ae0937ec3eea2c03603917da
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-06 18:36:13 +0300

    Edit guard function

--- src/app/guards/auth-guard.guard.ts
+++ src/app/guards/auth-guard.guard.ts
@@ -1,5 +1,9 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuardGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+
+  const router = inject(Router)
+  const isLoggedIn = !!localStorage.getItem('user')
+  return isLoggedIn || router.createUrlTree(['/signup'])
+}
