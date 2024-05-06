8f1229797f033193499faa69c0709629be6cf6ef
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-06 20:17:33 +0300

    Add functionality to cmp

--- src/app/cmps/transfer-fund/transfer-fund.component.ts
+++ src/app/cmps/transfer-fund/transfer-fund.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { Contact } from '../../models/contact.model';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { ContactService } from '../../services/contact.service';
 
 @Component({
   selector: 'transfer-fund',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './transfer-fund.component.scss'
 })
 export class TransferFundComponent {
+
+  @Input() contact!: Contact
+  private userService = inject(UserService)
+  private fb = inject(FormBuilder)
+  transferForm!: FormGroup
+
+  constructor() {
+    this.transferForm = this.fb.group({
+      amount: ['']
+    })
+  }
+
+  onSubmitTransfer() {
+    this.userService.addMoves(this.contact, this.transferForm.value.amount)
+  }
 
 }
