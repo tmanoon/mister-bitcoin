fce3cfb3e7763a3d58a2b8e6572dfc78c64eb040
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-06 17:01:05 +0300

    Add move interface

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -2,4 +2,11 @@ export interface User {
     name: string,
     coins: number,
     moves: string[]
-    }
+}
+
+export interface Move {
+    toId: string,
+    to: string,
+    at: number,
+    amount: number
+}
