4b3ba00da4378e740d48a79fb7d439fcf734ae83
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-06 21:40:14 +0300

    Edit function name and add router

--- src/app/cmps/transfer-fund/transfer-fund.component.ts
+++ src/app/cmps/transfer-fund/transfer-fund.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Contact } from '../../models/contact.model';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { ContactService } from '../../services/contact.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'transfer-fund',
   templateUrl: './transfer-fund.component.html',
   styleUrl: './transfer-fund.component.scss'
 })
-export class TransferFundComponent {
 
+export class TransferFundComponent {
+  router = inject(Router)
   @Input() contact!: Contact
+  
   private userService = inject(UserService)
   private fb = inject(FormBuilder)
   transferForm!: FormGroup
@@ -22,8 +25,8 @@ export class TransferFundComponent {
     })
   }
 
-  onSubmitTransfer() {
+  onTransferCoins() {
     this.userService.addMoves(this.contact, this.transferForm.value.amount)
+    this.router.navigateByUrl('/contact')
   }
-
 }
