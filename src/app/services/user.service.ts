0ef9e7a09b9f1906ddab7c8611d9a2d17ac6c193
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-06 21:38:11 +0300

    Remove extra spaces edit function to modify both user and moves and remove unnecessary data

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,10 +11,6 @@ const ENTITY = 'user'
 
 export class UserService {
 
-  randNames: string[] = ['Pedro', 'Salvador', 'Mariano', 'Emilia', 'Florencia', 'Carolina', 'Cris', 'Nicolas', 'Candela', 'Agustin']
-  randFamilyNames: string[] = ['Oppenheimer', 'Martinez', 'Gubler', 'Moreno', 'Vazquez', 'Fernandez', 'Inchausti', 'Esposito']
-  randCoinsAmount: number[] = [100, 4_000, 5_000_000, 200, 700, 20, 1000, 400, 378, 1_900, 9_000_000]
-
   constructor() { }
 
   public getUser(): User {
@@ -22,24 +18,29 @@ export class UserService {
     return user
   }
 
-  public signup(name: Partial<User>) {
-    const userToSave = {
+  public signup(name: string) {
+    const userToSave: User = {
       name,
       coins: 100,
       moves: []
     }
     localStorage.setItem(ENTITY, JSON.stringify(userToSave))
   }
-  
+
   logout() {
     localStorage.clear()
   }
 
-  public addMoves(contact: Contact, amount: string): Move {
-    const userToUpdate = this.getUser()  
+  public addMoves(contact: Contact, amount: string) {
+    const userToUpdate = this.getUser()
+    const move = this.getMove(contact, amount)
     userToUpdate.coins -= +amount
+    userToUpdate.moves.push(move)
     localStorage.setItem(ENTITY, JSON.stringify(userToUpdate))
+  }
 
+
+  private getMove(contact: Contact, amount: string) {
     return {
       toId: contact._id,
       to: contact.name,
